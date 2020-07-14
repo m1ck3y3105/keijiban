006b4cf31d01d83c2c90587b4b15f1bd84878d8d
mal1313 <ih.monsterz@icloud.com>
2020-07-14 22:28:15 +0900

    検索のCSSの変更

--- source/reserch.php
+++ source/reserch.php
@@ -45,37 +45,39 @@
         </script>
     </head>
     <div id="header"></div>
-    <div style="text-align: center;">
-        <div class="tile2">
-            <form action="" method="post" name="reserch-form">
-                <body id="reserch">
-                    <input id="inputkeyword" type="text" name="reserchkey" placeholder="キーワードを入力">
-                    <input id="searchbtn" type="submit" value="検索" onclick="Set_searchkey()">
-                </body>
-            </form>   
+    <div class="timer">
+        <div style="text-align: center;">
+            <div class="tile2">
+                <form action="" method="post" name="reserch-form">
+                    <body id="reserch">
+                        <input id="inputkeyword" type="text" name="reserchkey" placeholder="キーワードを入力">
+                        <input id="searchbtn" type="submit" value="検索" onclick="Set_searchkey()">
+                    </body>
+                </form>   
+            </div>
+        </div>
+        <div class="search_res">
+        <?php echo $key."の検索結果 : ".$hit."件" ?>
+        </div>
+        <div class="time">
+            <div class="time2">
+                <label>時間指定 </label>
+                <input type="radio"  name="time" onclick="func1()"checked id="r1"><label for="r1">なし</label>
+                <input type="radio" name="time"onclick="func2()" id="r2"><label for="r2"> あり</label>
+                <input type="datetime-local"id="not"disabled="disabled">～<input type="datetime-local"id="no"disabled="disabled">
+            </div>
+            <script>
+            function func1() {
+                document.getElementById("not").disabled = true;
+                document.getElementById("no").disabled = true;
+            }/*アリとなしの選択*/
+            function func2() {
+                document.getElementById("not").disabled = false;
+                document.getElementById("no").disabled = false;
+            }
+            </script>
         </div>
     </div>
-    <div class="search_res">
-        <?php echo $key."の検索結果 : ".$hit."件" ?>
-    </div>
-    <div class="time">
-                <div class="time2">
-                <label>時間指定 </label>
-                    <input type="radio"  name="time" onclick="func1()"checked id="r1"><label for="r1">なし</label>
-                    <input type="radio" name="time"onclick="func2()" id="r2"><label for="r2"> あり</label>
-                    <input type="datetime-local"id="not"disabled="disabled">～<input type="datetime-local"id="no"disabled="disabled">
-                </div>
-                <script>
-                function func1() {
-                    document.getElementById("not").disabled = true;
-                    document.getElementById("no").disabled = true;
-                }/*アリとなしの選択*/
-                function func2() {
-                    document.getElementById("not").disabled = false;
-                    document.getElementById("no").disabled = false;
-                }
-                </script>
-            </div>
     <div class="tile3">
         <div class="sortmenu">
             <h2>ソート順</h2>
