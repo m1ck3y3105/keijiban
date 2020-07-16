4647b21dee26d459c396a910580ec84d8a179bc3
m1ck3y3105 <cookie09253124@gmail.com>
2020-07-16 11:28:48 +0900

    header.html 削除に伴い、各ページのURLを変更

--- source/change_name.php
+++ source/change_name.php
@@ -12,7 +12,7 @@
         <script src="//code.jquery.com/jquery-2.2.4.min.js"></script>
         <script>
             $(function () {
-                $("#header").load("./header.html");
+                $("#header").load("./header.php");
                 $("#footer").load("./footer.html");
             });
         </script>
