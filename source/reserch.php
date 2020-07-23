cdeb5ccbcf9c84afa4cc97532475f94ed31f54bc
m1ck3y3105 <cookie09253124@gmail.com>
2020-07-23 22:49:12 +0900

    検索画面の検索フォームの見た目を変更

--- source/reserch.php
+++ source/reserch.php
@@ -28,6 +28,7 @@
         <meta charset="UTF-8">
         <title>スレッド検索</title>
         <link rel="stylesheet" href="styles/style.css">
+        <link rel="stylesheet" href="styles/style2.css">
         <link rel="stylesheet" href="styles/tiles.css">
         <script src="//code.jquery.com/jquery-2.2.4.min.js"></script>
         <script>
@@ -57,40 +58,37 @@
     </head>
     <div id="header"></div>
     <div style="text-align: center;">
-        <div class="tile2">
-            <form action="" method="post" name="reserch-form">
-                <body id="reserch">
-                    <div class="searchmenu">
-                        <h2>検索対象</h2>
-                        <input type="radio" name="s_target">スレッド</input>
-                        <input type="radio" name="s_target">レス</input>
-                    </div>
-                    <input id="inputkeyword" type="text" name="reserchkey" placeholder="キーワードを入力">
-                    <input id="searchbtn" type="submit" value="検索" onclick="Set_searchkey()">
-                </body>
-            </form>   
-        </div>
+        <div class="timer">
+            <div class="tile2">
+                <form action="source/reserch.php" method="get" name="reserch-form">
+                    <body id="reserch">
+                            <input id="inputkeyword" type="text" name="reserchkey" placeholder="キーワードを入力">
+                            <input id="searchbtn" type="button" value="検索" onclick="Set_searchkey()">
+                    </body>
+                </form> 
+            </div>
+            <div class="time">
+                <div class="time2">
+                    <label>時間指定 :  </label>
+                    <input type="radio"  name="time" onclick="func1()"checked id="r1"><label for="r1">なし</label>
+                    <input type="radio" name="time"onclick="func2()" id="r2"><label for="r2">あり</label>
+                    <input type="datetime-local"id="not"disabled="disabled">～<input type="datetime-local"id="no"disabled="disabled">
+                </div>
+                <script>
+                    function func1() {
+                        document.getElementById("not").disabled = true;
+                        document.getElementById("no").disabled = true;
+                    }
+                    function func2() {
+                        document.getElementById("not").disabled = false;
+                        document.getElementById("no").disabled = false;
+                    }
+                </script>
+            </div>
+        </div>   
     </div>
     <div class="search_res">
         <?php echo $key."の検索結果 : ".$hit."件" ?>
-    </div>
-    <div class="time">
-        <div class="time2">
-        <label>時間指定 </label>
-            <input type="radio"  name="time" onclick="func1()"checked id="r1"><label for="r1">なし</label>
-            <input type="radio" name="time"onclick="func2()" id="r2"><label for="r2"> あり</label>
-            <input type="datetime-local"id="not"disabled="disabled">～<input type="datetime-local"id="no"disabled="disabled">
-        </div>
-        <script>
-        function func1() {
-            document.getElementById("not").disabled = true;
-            document.getElementById("no").disabled = true;
-        }
-        function func2() {
-            document.getElementById("not").disabled = false;
-            document.getElementById("no").disabled = false;
-        }
-        </script>
     </div>
     <div class="tile3">
         <div class="sortmenu">
@@ -105,7 +103,6 @@
             </p>
         </div>
 
-               
         <?php
         if($hit>0){
             while($row = pg_fetch_row($result1)){
