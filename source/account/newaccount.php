9c73c86be45f79ed8a764eb61245f8aa68d21e3a
texyaku <67852829+texyaku@users.noreply.github.com>
2020-07-28 22:39:34 +0900

    ようたが作ったデザインを反映

--- source/account/newaccount.php
+++ source/account/newaccount.php
@@ -2,12 +2,32 @@
 <html lang="ja">
 <head>
     <base href="/"></base>
+    <title>掲示板サイト</title>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/yota.css">
+    <script src="//code.jquery.com/jquery-2.2.4.min.js"></script>
+    <script>
+        $(function () {
+            $("#header").load("source/header.php");
+            $("#footer").load("source/footer.html");
+        });
+    </script>
 </head>
 <body>
-<form action="source/account/newaccount_check.php" method="POST">
-    <p>ユーザID：<input name="user_name" type="text"></p>
-    <p>パスワード：<input name="password" type="password"></p>
-    <p>パスワード再入力：<input name="re_password" type="password"></p>
-    <input name="create_account" type="submit" value="アカウント作成">
-</form>
+    <div id="header"></div>
+    <div id="newac">
+        <form action="source/account/newaccount_check.php" method="POST">
+            <div class="loginbox">
+                <h3>ユーザID：<input name="user_name" type="text"></h3>
+                <h3>パスワード：<input name="password" type="password"></h3>
+                <h3>パスワード再入力：<input name="re_password" type="password"></h3>
+                <div class="submitbtn">
+                    <input name="create_account" type="submit" value="アカウント作成">
+                </div>
+            </div>
+        </form>
+    </div>
+<div id="footer"></div>
+</body>
+</html>
