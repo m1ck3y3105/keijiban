e9300713ed63eca8bf2e0cb0b845866661f0cb1a
texyaku <67852829+texyaku@users.noreply.github.com>
2020-07-28 22:42:49 +0900

    ようたが作ったデザインを反映

--- source/account/newaccount_check.php
+++ source/account/newaccount_check.php
@@ -1,36 +1,79 @@
 <?php
   session_start();
+  $OK = 0;
   
   /*入力の項目が足りるか*/
   if(!empty($_POST["user_name"]) && !empty($_POST["password"]) && !empty($_POST["re_password"])){
-    $user_name = $_POST["user_name"];
-    $password = $_POST["password"];
-    $re_password = $_POST["re_password"];
+      $OK = 1;
+      $user_name = $_POST["user_name"];
+      $password = $_POST["password"];
+      $re_password = $_POST["re_password"];
 
     
-    /*再入力されたパスワードのチェック*/
-    if($password == $re_password){
-        error_reporting(0);
-
-        $connect=pg_connect("dbname=postgres user=postgres password=KMtkm1412");
-        $sql1="INSERT INTO user_admin(user_name,login_pass) VALUES('{$user_name}','{$password}')";
-        $result1 = pg_query($connect,$sql1);
-
-        /*ユーザidがかぶってないか*/
-        if(!$result1){
-            echo "そのユーザidはすでにつかわれています。";
-        }
-        else{
-            echo "新規登録できました。";
-        }
-    }
-    else{
-        echo "再入力されたパスワードが違います。";
-    }
+      /*再入力されたパスワードのチェック*/
+      if($password == $re_password){
+          $OK = 2;
+          error_reporting(0);
+ 
+          $connect=pg_connect("dbname=postgres user=postgres password=KMtkm1412");
+          $sql1="INSERT INTO user_admin(user_name,login_pass) VALUES($1,$2)";
+          $array1 = array("user_name" => "{$user_name}", "password" => "{$password}");
+          $result1 = pg_query_params($connect,$sql1,$array1);
+ 
+          /*ユーザidがかぶってないか*/
+          if($result1){
+              $OK = 3;
+          }
+      }
   }
-  else{
-      echo "入力の項目が足りていません。";
-  }
-  echo '<a class="button1" href="../index.html">トップへ</a>';
 
 ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <base href="/"></base>
+    <title>掲示板サイト</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/yota.css">
+    <script src="//code.jquery.com/jquery-2.2.4.min.js"></script>
+    <script>
+        $(function () {
+            $("#header").load("source/header.php");
+            $("#footer").load("source/footer.html");
+        });
+    </script>
+</head>
+<body>
+    <div id="header"></div>
+
+    <div id="loginch">
+
+    <?php if($OK == 0){ ?>
+    <!-- 入力が足りていないとき＋直接遷移されたときの表示 -->
+    <h2>入力が足りていません</h2>
+    <a class="button1" href="source/account/newaccount.php">アカウント作成画面へ</a>
+
+    <?php }else if($OK == 1){ ?>
+    <!-- 再入力されたパスワードが違った場合の表示 -->
+    <h2>再入力されたパスワードが違います</h2>
+    <a class="button1" href="source/account/newaccount.php">アカウント作成画面へ</a>
+    
+
+    <?php }else if($OK == 2){ ?>
+    <!-- ユーザIDが既に使われていたものだった時の表示 -->
+    <h2>そのユーザIDは既に使われています</h2>
+    <a class="button1" href="source/account/newaccount.php">アカウント作成画面へ</a>
+
+    <?php }else if($OK == 3){ ?>
+    <!-- 正常にアカウント作成できた時の表示 -->
+    <h2>新規アカウント登録ができました</h2>
+    <a class="button1" href="source/index.html">トップへ</a>
+    <?php } ?>
+
+    </div>
+  
+    <div id="footer"></div>
+</body>
+</html>
